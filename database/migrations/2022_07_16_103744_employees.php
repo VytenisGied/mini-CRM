8148be0d8b3b79ee5dc9f2ff425ef2ade11285b9
auxtheias <vyenis.gied@gmail.com>
2022-07-16 13:55:50 +0300

    added migrations

---
+++ database/migrations/2022_07_16_103744_employees.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * CRUD functionality (Create / Read / Update / Delete) for two menu items: Companies and Employees.
+ * Employees DB table consists of these fields: first name (required), last name (required), Company (foreign key to Companies), email, phone.
+ */
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('employees', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('lastname');
+        $table->foreignId('company_id');
+        $table->string('email')->unique()->nullable();
+        $table->string('phone')->unique()->nullable();
+        $table->timestamps();
+
+        $table->foreign('company_id')->references('id')->on('companies');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('employees');
+    }
+};
