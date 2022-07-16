a290f300e0916bc8fb1deed0c0de9d141213ddee
auxtheias <vyenis.gied@gmail.com>
2022-07-16 12:35:16 +0300

    init

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+<script setup>
+import GuestLayout from '@/Layouts/Guest.vue';
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+})
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <GuestLayout>
+      <div>Welcome</div>
+    </GuestLayout>
+</template>
+
+<style scoped>
+    
+</style>
